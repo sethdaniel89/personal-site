98c0a19da4d28ab8fa2ddc8d9b2bb3bc4118af5d
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-08-10 09:12:20 -0400

    Styles exoerience cards.

--- app/assets/javascripts/mainpages.js
+++ app/assets/javascripts/mainpages.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+	$('.fancybox-thumbs').fancybox({
+	    prevEffect: 'none',
+	    nextEffect: 'none',
+
+	    closeBtn: true,
+	    arrows: false,
+	    nextClick: true,                
+
+	    helpers: {
+	        thumbs: {
+	            width : 50,
+	            height: 50
+	        }
+	    }
+	});
