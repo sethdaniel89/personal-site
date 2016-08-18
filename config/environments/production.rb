49d4c97ae5dd2fc30bce4cb54a4f0690314c79d6
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-08-18 14:10:30 -0400

    Adds herkou site name to production.rb file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  host = 'something-something-darkside.herokuapp.com'
+  host = 'stark-dawn-28127.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
